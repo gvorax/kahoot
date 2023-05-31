2317f20c1716d08ad73fec2856ddd77d7dec8a3a
gvorax <otabek83019@gmail.com>
2023-05-31 17:02:55 +0500

    fix

--- src/context/useContext.jsx
+++ src/context/useContext.jsx
@@ -28,7 +28,6 @@ export function UserContextProvider({ children }) {
     },
   ]);
 
-  console.log("news", news);
   return (
     <UserContext.Provider
       value={{ user, setUser, hostUser, setHostUser, quizs, setQuizs,news, setNews }}
